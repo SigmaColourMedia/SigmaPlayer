88ee452dd9fb831dabd7bb10bc182c9e73e2d940
szymakula <szymakula@gmail.com>
2024-09-08 21:59:56 +0200

    handle disabled drawer button

--- frontend/src/watch/drawer.ts
+++ frontend/src/watch/drawer.ts
@@ -1,19 +1,30 @@
-import { NotificationState, NotificationSubscriber } from "./main";
+import { NotificationState, NotificationSubscriber, Watch } from "./main";
 import { RoomData } from "../api";
 import { html, nothing } from "lit";
 import { ChevronUpIcon } from "../icons/chevronUpIcon";
 import { RoomCarousel } from "../home/roomCarousel";
 
-export function DrawerManager(subscriber: NotificationSubscriber) {
+export function DrawerManager(this: Watch, subscriber: NotificationSubscriber) {
   switch (subscriber.state) {
     case NotificationState.Open:
-      return Drawer(subscriber.data);
+      return Drawer.call(this, subscriber.data);
     default:
       return nothing;
   }
 }
 
-function Drawer(roomData: RoomData) {
+function Drawer(this: Watch, roomData: RoomData) {
+  const hasNoRooms =
+    roomData.filter((room) => room.id !== Number(this.id)).length == 0;
+
+  if (hasNoRooms)
+    return html`<div class="drawer-box">
+      <button title="Open drawer" class="drawer-button">
+        ${ChevronUpIcon}
+      </button>
+      <input disabled class="drawer-input" type="checkbox" />
+    </div>`;
+
   return html`<div class="drawer-box">
     <button title="Open drawer" class="drawer-button">${ChevronUpIcon}</button>
     <input class="drawer-input" type="checkbox" />
