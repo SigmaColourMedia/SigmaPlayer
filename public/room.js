47fab1c0f4613140be02f9a9fab06fb1d39921fb
redkor <redkor910.97@gmail.com>
2025-12-22 20:44:21 +0100

    feat: add reconnect

--- public/room.js
+++ public/room.js
@@ -1,8 +1,11 @@
-const video = document.querySelector("video");
-const viewerCounter = document.getElementById("viewer-counter");
-const pageID = location.pathname.split("/")[2];
-const offlineDialog = document.getElementById("offline-dialog");
-const errorDialog = document.getElementById("error-dialog");
+const video = document.querySelector('video');
+const viewerCounter = document.getElementById('viewer-counter');
+const pageID = location.pathname.split('/')[2];
+const offlineDialog = document.getElementById('offline-dialog');
+const errorDialog = document.getElementById('error-dialog');
+const loader = document.querySelector('.loader');
+
+let retries = 0;
 
 function connectSSE() {
   const sse = new EventSource(`/events/room?id=${pageID}`);
@@ -11,7 +14,7 @@ function connectSSE() {
     const data = JSON.parse(event.data);
 
     switch (data.event) {
-      case "room_update": {
+      case 'room_update': {
         viewerCounter.textContent = data.payload.viewer_count;
         break;
       }
@@ -23,7 +26,7 @@ function connectSSE() {
 }
 
 function showSteamCloseDialog() {
-  video.poster = "/static/fallback.png";
+  video.poster = '/static/fallback.png';
   video.srcObject = null;
   video.src = null;
   offlineDialog.showModal();
@@ -31,45 +34,69 @@ function showSteamCloseDialog() {
 
 async function startWhep() {
   const conn = new RTCPeerConnection();
-  conn.addTransceiver("audio", { direction: "recvonly" });
-  conn.addTransceiver("video", { direction: "recvonly" });
+  console.log(conn);
+
+  conn.addTransceiver('audio', { direction: 'recvonly' });
+  conn.addTransceiver('video', { direction: 'recvonly' });
   conn.ontrack = ({ streams }) => {
     video.srcObject = streams[0];
   };
 
-  conn.onconnectionstatechange = () => {
+  conn.addEventListener('connectionstatechange', () => {
+    retries++;
     switch (conn.connectionState) {
-      case "closed": {
+      case 'closed': {
+        if (retries < 5) {
+          startWhep().then(() => {
+            video.play();
+          });
+        } else {
+          showSteamCloseDialog();
+        }
+        break;
+      }
+      case 'disconnected': {
         showSteamCloseDialog();
         break;
       }
-      case "disconnected": {
-        showSteamCloseDialog();
+      case 'failed': {
+        if (retries < 5) {
+          startWhep().then(() => {
+            video.play();
+          });
+        } else {
+          showSteamCloseDialog();
+        }
         break;
       }
-      case "failed": {
-        showSteamCloseDialog();
+      case 'connecting': {
+        loader.classList.toggle('hidden');
+        break;
+      }
+      case 'connected': {
+        loader.classList.toggle('hidden');
+        retries = 0;
         break;
       }
     }
-  };
+  });
 
   const sdp_offer = await conn.createOffer();
   await conn.setLocalDescription(sdp_offer);
 
   const sdp_answer = await fetch(`/whep?target_id=${pageID}`, {
-    method: "POST",
+    method: 'POST',
     headers: {
-      "content-type": "application/sdp",
+      'content-type': 'application/sdp',
     },
     body: sdp_offer.sdp,
   }).then((res) => res.text());
 
-  await conn.setRemoteDescription({ sdp: sdp_answer, type: "answer" });
+  await conn.setRemoteDescription({ sdp: sdp_answer, type: 'answer' });
 }
 
 connectSSE();
 startWhep().catch((err) => {
-  console.error("RTC setup error: ", err);
+  console.error('RTC setup error: ', err);
   errorDialog.showModal();
 });
